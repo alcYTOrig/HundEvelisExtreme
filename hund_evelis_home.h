0dd344109d15e3b3a69166010e90a5c7cd15e043
ALC <craftag00@gmail.com>
2025-12-15 22:30:11 +0300

    Alpha V1.1 - Исправлены баги - Поддержка русского языка - Небольшие доработки

--- hund_evelis_home.h
+++ hund_evelis_home.h
@@ -7,17 +7,19 @@
 
 #include <iostream>
 #include <string>
-#include <stdlib.h>
-
+#include <map>
+#include <algorithm>
 
 using namespace std;
-
 
 
 class AmoreHundEvelis {
 private:
     string nameUser;
     bool isStarted = true;
+    // История команд
+    map<int, string> historyCommands_and_id;
+    int history_last_id = 0;
 public:
     AmoreHundEvelis() {
         char* _nameUser = getenv("USERNAME");
@@ -27,23 +29,38 @@ public:
     }
 
     void print_welcome_text() {
-        cout << "\033[32mAmore PiRasNat\033[0m \033[1;31m5\033[0m | \033[34mWELCOME BACK\033[0m\n\n" << endl << endl;
+        cls();
+        cout << "\033[1;32mAmore \033[1;34mHundEvelis\033[0m\033[1;31mExtreme\033[0m | \033[1;34mWELCOME BACK\033[0m\n\n" << endl << endl;
     }
 
-    void clear_screen() {
+    void cls() {
         system("cls");
+    }
+
+    void print_history() {
+        for (const auto& command : historyCommands_and_id) {
+            cout << "ID: " << command.first << " - " << "'" << command.second << "'" << endl;
+        }
     }
 
     void while_event_handler() {
         while (isStarted) {
-            cout << "\033[34m" << nameUser << ":\033[0m \033[32m$\033[0m\033[34m~\033[0m ";
-            string input;
-            getline(cin, input);
-            if (input.empty()) continue;
-            if (input == "exit") isStarted = false;
-            if (input == "cls") clear_screen();
+            try {
+                cout << "\033[34m" << nameUser << ":\033[0m \033[32m$\033[0m\033[34m~\033[0m ";
+                string input;
+                getline(cin, input);
+                if (input.empty()) continue;
+                historyCommands_and_id[history_last_id] = input;
+                history_last_id++;
+                if (input == "exit") isStarted = false;
+                if (input == "history") print_history();
+                if (input == "cls") cls();
+                if (input == "exception") throw runtime_error("TEST ERROR");
+            }
+            catch (const exception& exception) {
+                cout << "\033[1;31mУпс.. Произошла непредвиденая ошибка" << endl;
+            }
         }
-        cout << "\033[1;31mExiting...";
-        exit(0);
+        cout << "\033[1;31mExiting...\033[0m";
     }
 };
--- main.cpp
+++ main.cpp
@@ -3,9 +3,49 @@
 // Update by Amore (ALC) on 14.12.2025
 //
 
+#include <chrono>
+#include <functional>
+#include <windows.h>
+#include <iostream>
 #include "hund_evelis_home.h"
 
+
+auto run_and_time(std::function<void()> fn)
+{
+//    std::chrono::high_resolution_clock timer;
+//    auto t0 = timer.now();
+//    auto t1 = timer.now();
+    auto t0 = std::chrono::system_clock::now();
+
+    fn();
+
+    auto t1 = std::chrono::system_clock::now();
+    return t1 - t0;
+//    auto diff_ms = std::chrono::duration_cast<std::chrono::milliseconds>(diff).count();
+}
+
+long int time_ms(auto diff)
+{
+    return std::chrono::duration_cast<std::chrono::milliseconds>(diff).count();
+}
+
+void test1()
+{
+    int a = 0;
+    for (int i = 0; i < 100000; i++)
+    {
+        a += i * 2;
+    }
+}
+
 int main() {
+
+    auto time_test1 = run_and_time(test1);
+    auto time_test1_ms = time_ms(time_test1);
+
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
     AmoreHundEvelis hundEvelis;
+    exit(0);
 }
 
